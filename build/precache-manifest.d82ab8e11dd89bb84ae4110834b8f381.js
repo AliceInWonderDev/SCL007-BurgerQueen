7982ca6c2473d05e23600a37d1b6c5332c1eb797
AliceInWonderDev <ali.castaneda05@gmail.com>
2019-03-06 22:45:51 -0300

    first commit, including basics folders

---
+++ build/precache-manifest.d82ab8e11dd89bb84ae4110834b8f381.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7bd50fcdd1e3fde6c8c",
+    "url": "/static/css/main.98566c00.chunk.css"
+  },
+  {
+    "revision": "e7bd50fcdd1e3fde6c8c",
+    "url": "/static/js/main.e7bd50fc.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.a539ea9d.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "961cf3df8de6d7ecaae95a28e13f23f8",
+    "url": "/index.html"
+  }
+];
